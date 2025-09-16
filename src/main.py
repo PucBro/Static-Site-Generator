66452b6b44fc28067669e6e9980de3468b3788a5
Diego <diegoo9997@gmail.com>
2025-09-15 20:42:17 -0600

    Reformulate Code in function copy files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 from copyfiles import copy_files
 from generate_page import generate_page, generate_pages_recursive
+import sys, os
+import shutil
+
 
 
 def main():
-    copy_files()
+    if sys.argv[0]:
+        base_path = sys.argv[0]
+    else:
+        base_path = "/"
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+        os.mkdir("./public")
+    copy_files(source_path="./static", destiny_path="./public")
     generate_pages_recursive("./content", "./template.html", "./public")
     
 
