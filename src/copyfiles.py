eb670475f6b250774c55c5023b9fab846ff1292e
Diego <diegoo9997@gmail.com>
2025-09-15 21:22:06 -0600

    Try to deploy the site in github pages

--- src/copyfiles.py
+++ src/copyfiles.py
@@ -13,6 +13,7 @@ def copy_files(source_path, destiny_path):
             shutil.copy( element_path,path_to_copy)
         else:
             new_destiny_path = os.path.join(destiny_path, element)
+            os.mkdir(new_destiny_path)
             copy_files(source_path=element_path, destiny_path=new_destiny_path)
 
 
